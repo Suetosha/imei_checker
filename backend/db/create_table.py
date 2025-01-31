dcd5fe73b4ce89a5a519ed7906b9cb03169abedd
Suetosha <honey.pancake2017@yandex.ru>
2025-01-31 17:06:16 +0100

    Added create table and connection to db functions

---
+++ backend/db/create_table.py
@@ -0,0 +1,15 @@
+from backend.db.db_connection import get_db_connection
+
+
+def create_table():
+    conn = get_db_connection()
+    conn.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT UNIQUE NOT NULL,
+            password TEXT NOT NULL
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
